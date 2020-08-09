e9a7b76fbf07e655f75e30d232f6aba1bbb80fcf
Keshav <keshavagarwal66@gmail.com>
2020-08-10 01:34:15 +0530

    2nd commmit

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,3 +139,8 @@ LOGIN_REDIRECT_URL = '/'
 
 
 LOGIN_URL = '/accounts/login/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

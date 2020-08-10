b5705ed33c0fae115e8c80bf8c806787fb031c22
Keshav-Agarwal <keshavagarwal66@gmail.com>
2020-08-10 19:34:54 +0530

    Update settings.py

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "/tracker/static/"),
 )
 
 STATIC_URL = '/tracker/static/'
